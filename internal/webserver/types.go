6f034a8e2e1d8cc91e0c17b638d08f20262ed92f
Cosmin <cosmin.mocanu97@yahoo.com>
2021-05-14 16:49:25 +0300

    added docker postgres init file

--- internal/webserver/types.go
+++ internal/webserver/types.go
@@ -2,12 +2,10 @@ package webserver
 
 import (
 	"database/sql"
-	"github.com/CosminMocanu97/dissertationBackend/internal/mail"
 )
 
 type Service struct {
 	Version        string
 	Database       *sql.DB
 	JwtSecret      string
-	MailingService mail.Mailer
 }
--- internal/webserver/webserver.go
+++ internal/webserver/webserver.go
@@ -3,21 +3,20 @@ package webserver
 import (
 	"net/http"
 
-	"github.com/CosminMocanu97/dissertationBackend/internal/auth"
 	"github.com/CosminMocanu97/dissertationBackend/pkg/log"
+	"github.com/bwmarrin/snowflake"
 	"github.com/gin-gonic/gin"
 	cors "github.com/rs/cors/wrapper/gin"
-
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-const (
-	LOGIN_CREDENTIALS_BINDING_ERROR = "Could not bind request data"
-	CREDENTIALS_NOT_VERIFIED_ERROR  = "Could not finish credentials verification"
-	INVALID_CREDENTIALS_ERROR       = "Invalid credentials"
-	USER_ID_COULD_NOT_BE_RETRIEVED  = "Error retrieving the id for the user"
-)
+func InitiateSnowflakeNode() *snowflake.Node {
+	node, err := snowflake.NewNode(1)
+	if err != nil {
+		log.Fatal("Error initializing snowflake %s", err)
+	}
+
+	return node
+}
 
 var (
 	node = InitiateSnowflakeNode()
@@ -34,61 +33,22 @@ func Api(s *Service) *gin.Engine {
 		context.Status(http.StatusOK)
 	})
 	r.GET("/ping", s.HandleGetPingRequest)
-	r.POST("/mail", s.HandlePostMail)
-	r.POST("/register", s.HandlePostRegisterRequest)
-	r.POST("/login", s.HandlePostLoginRequest)
-	r.GET("/activate/:token", s.HandlePostActivateAccount)
-	r.POST("/forgot-password", s.HandlePostForgotPasswordRequest)
-	r.POST("/renew-password/:token", s.HandlePostRenewPasswordRequest)
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	r.GET("/test", s.HandleGetTestRequest)
 	return r
 }
 
-// JWT authorisation middleware
-
-func AuthorizeJWT() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		tokenString := c.GetHeader("Authorization")
-		// if the token doesn't exist, return unauthorised
-		if len(tokenString) == 0 {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"error": "No authorization token in the request header",
-			})
-		}
-		token, err := auth.JWTAuthService().ValidateToken(tokenString)
-		if err != nil {
-			log.Error("Error validating the JWT token %s: %s", tokenString, err)
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-		if token.Valid {
-			claims := token.Claims.(*auth.AuthCustomClaims)
-			log.Info("token email: %s", claims.Email)
-			c.Set("claims", claims)
-			c.Next()
-		} else {
-			log.Error("%s", err)
-			c.AbortWithStatus(http.StatusUnauthorized)
-		}
-	}
-}
-
-//login contorller interface
-type LoginController interface {
-	Login(ctx *gin.Context) string
-}
-
-// handle GET "/ping" request
-// HandleGetPingRequest godoc
-// @Summary ping - pong
-// @Description api health endpoint
-// @Accept  json
-// @Produce json
-// @Success 200 {string} pong
-// @Failure default {json} http.StatusInternalServerError
-// @Router /ping [get]
 func (s *Service) HandleGetPingRequest(c *gin.Context) {
 	id := node.Generate()
 
 	log.Info("UUID: %s; Request to GET /ping", id)
 	c.String(http.StatusOK, "pong")
+}
+
+func (s *Service) HandleGetTestRequest(c *gin.Context) {
+	id := node.Generate()
+
+	log.Info("UUID: %s; Request to GET /ping", id)
+	c.JSON(http.StatusOK, gin.H{
+		"something": "test",
+	})
 }
